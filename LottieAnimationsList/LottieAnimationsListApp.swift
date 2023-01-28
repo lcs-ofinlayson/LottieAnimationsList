af1e0301b0b35f37664b7ee83352b986456761b6
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-28 10:00:09 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Oliver Finlayson on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
